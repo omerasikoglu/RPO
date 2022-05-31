4d60c8a979a243927cd471240c8103b0afd1b6b5
OmerAsik <faruk.asikoglu@gmail.com>
2022-05-31 23:34:13 +0300

    Updated Modular General Input System

---
+++ Assets/Game/Scripts/GeneralSystem/GeneralInputFPS.cs
@@ -0,0 +1,52 @@
+using DG.Tweening;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class GeneralInputFPS : MonoBehaviour {
+   
+    [SerializeField] private InputManager inputManager;
+    [SerializeField] private List<GeneralMovePoint> movePointList;
+    [SerializeField] private Transform general;
+
+    private int currentRoad = 1;
+
+    private void OnEnable() => inputManager.OnSwipePerformed += MoveWithSwipe;
+    public void OnDisable() => inputManager.OnSwipePerformed -= MoveWithSwipe;
+
+    [SerializeField] private float deltaThreshold = 50f;
+    private bool canInput = true;
+    private void MoveWithSwipe(Vector2 delta) { //FPS Shoulder Cam
+
+        if (!canInput) return;
+        if (Mathf.Abs(delta.x) < deltaThreshold) return;
+
+        switch (delta.x) {
+            //right swipe
+            case > 0f when currentRoad == movePointList.Count: //if you stand top right
+            Jump(movePointList.Last().Position);
+            break;
+            case > 0f:
+            currentRoad++;
+            Jump(movePointList[currentRoad - 1].Position);
+            break;
+            //left swipe
+            case < 0f when currentRoad == 1: //if you stand top left
+            Jump(movePointList.First().Position);
+            break;
+            case < 0f:
+            currentRoad--;
+            Jump(movePointList[currentRoad - 1].Position);
+            break;
+        }
+
+        void Jump(Vector3 movePos, float duration = 1f) {
+            general.DOJump(movePos, 2f, 1, duration);
+            canInput = false;
+            StartCoroutine(UtilsClass.Wait(() =>
+            {
+                canInput = true;
+            }, duration));
+        }
+    }
+}
---
+++ Assets/Game/Scripts/GeneralSystem/GeneralInputRTS.cs
@@ -0,0 +1,33 @@
+using DG.Tweening;
+using System.Linq;
+using UnityEngine;
+
+public class GeneralInputRTS : MonoBehaviour {
+
+    [SerializeField] private InputManager inputManager;
+    [SerializeField] private Transform general;
+
+    private int currentRoad = 1;
+
+    private bool isTouching => inputManager.IsTouching;
+    private RaycastHit[] hits = new RaycastHit[5];
+    private void Update() => MoveWithColliders();
+
+    private void MoveWithColliders() { //RTS Cam
+        if (!isTouching) return;
+        Ray ray = UtilsClass.GetScreenPointToRay(inputManager.TouchCoords);
+
+        int hitCount = Physics.RaycastNonAlloc(ray, hits, Mathf.Infinity);
+        var results = hits.Take(hitCount);
+
+        bool isTouchable = results.Any(o => o.transform.CompareTag(StringData.TOUCHABLE));
+        if (!isTouchable) return;
+
+        GeneralMovePoint point = results.First().transform.GetComponentInParent<GeneralMovePoint>();
+        
+        //if (currentRoad == point.Road) return;
+
+        currentRoad = (int)point.Road;
+        general.DOJump(point.Position, 3f, 1, 1f).SetEase(Ease.Linear);
+    }
+}
